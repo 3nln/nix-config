489edd2353347b50c8e363af655d1ac5758bd44f
Javohir G'ulomjonov <javohirtech@gmail.com>
2025-09-03 19:50:13 +0500

    chore: nix for arch

--- flake.nix
+++ flake.nix
@@ -3,55 +3,119 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      nixpkgs.config.allowUnfree = true;
+    macSystem = "aarch64-darwin";
+    linuxSystem = "x86_64-linux";
 
-      environment.systemPackages =
-        [
-          pkgs.kitty
-          pkgs.fastfetch
-          pkgs.fish
-          pkgs.telegram-desktop
-          pkgs.firefox
-          pkgs.zed-editor
-          pkgs.google-chrome
-        ];
+    # configuration = { pkgs, ... }: {
+    #   # List packages installed in system profile. To search by name, run:
+    #   # $ nix-env -qaP | grep wget
+    #   nixpkgs.config.allowUnfree = true;
 
-      fonts.packages =
-      [
-        pkgs.nerd-fonts.jetbrains-mono
-      ];
+    #   environment.systemPackages =
+    #     [
+    #       pkgs.kitty
+    #       pkgs.fastfetch
+    #       pkgs.fish
+    #       pkgs.telegram-desktop
+    #       pkgs.firefox
+    #       pkgs.zed-editor
+    #       pkgs.google-chrome
+    #     ];
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+    #   fonts.packages =
+    #   [
+    #     pkgs.nerd-fonts.jetbrains-mono
+    #   ];
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+    #   # Necessary for using flakes on this system.
+    #   nix.settings.experimental-features = "nix-command flakes";
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+    #   # Enable alternative shell support in nix-darwin.
+    #   # programs.fish.enable = true;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
+    #   # Set Git commit hash for darwin-version.
+    #   system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
+    #   # Used for backwards compatibility, please read the changelog before changing.
+    #   # $ darwin-rebuild changelog
+    #   system.stateVersion = 6;
+
+    #   # The platform the configuration will be used on.
+    #   nixpkgs.hostPlatform = "aarch64-darwin";
+    # };
+
   in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#mbp
+
+    # macos
     darwinConfigurations."mbp" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        {
+          nixpkgs.hostPlatform = macSystem;
+          nix.settings.experimental-features = "nix-command flakes";
+
+          environment.systemPackages = with import nixpkgs { system = macSystem; };[
+            kitty
+            fastfetch
+            telegram-desktop
+            fish
+            firefox
+            zed-editor
+            google-chrome
+          ];
+
+          system.stateVersion = 6;
+          
+        }
+
+        home-manager.darwinModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.neo = {
+            home.username = "neo";
+            home.homeDirectory = "/Users/neo";
+            home.stateVersion = "24.05";
+
+            programs.fish.enable = true;
+            programs.kitty.enable = true;
+          };
+        }
+       ];
+    };
+
+
+    # arch
+    homeConfigurations."neo" = home-manager.lib.homeManagerConfiguration {
+      pkgs = import nixpkgs {system = linuxSystem; };
+
+      modules = [
+        ({pkgs, import, ...}: {
+          home.username = "nnolan";
+          home.homeDirectory = "/home/nnolan";
+          home.stateVersion = "24.05";
+
+
+          programs.fish.enable = true;
+
+          programs.kitty.enable = true;
+          
+          home.packages = [
+            pkgs.kitty
+          ];
+        })
+      ];
     };
   };
 }
