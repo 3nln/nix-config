1640b4eb22279652072accc5431da58d9b33890b
neo <javohirtech@gmail.com>
2025-09-16 00:11:16 +0500

    feat: fastfetch logo changed

--- modules/home/fastfetch/default.nix
+++ modules/home/fastfetch/default.nix
@@ -3,32 +3,54 @@
   programs.fastfetch = {
     enable = true;
     settings = {
+      logo = {
+        source = ./logo;
+        padding = {
+          top = 3;
+          right = 3;
+          left = 3;
+        };
+      };
+      display = {
+        size = {
+      binaryPrefix = "si";
+    };
+        };
       modules = [
         {
           type = "datetime";
           key = "Date";
           format = "{1}-{3}-{11}";
         }
-        "title"
-        "os"
-        "host"
-        "kernel"
-        "uptime"
-        "packages"
-        "shell"
-        "display"
-        "de"
-        "wm"
-        "terminal"
-        "cpu"
-        "gpu"
-        "memory"
-        "swap"
-        "disk"
-        "localip"
-        "PublicIp"
-        "battery"
-        "locale"
+         "title"
+          "separator"
+          "os"
+          "host"
+          "kernel"
+          "uptime"
+          "packages"
+          "shell"
+          "display"
+          "de"
+          "wm"
+          "wmtheme"
+          "theme"
+          "icons"
+          "font"
+          "cursor"
+          "terminal"
+          "terminalfont"
+          "cpu"
+          "gpu"
+          "memory"
+          "swap"
+          "disk"
+          "localip"
+          "battery"
+          "poweradapter"
+          "locale"
+          "break"
+          "colors"
       ];
     };
   };
