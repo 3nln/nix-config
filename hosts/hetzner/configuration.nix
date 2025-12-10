a3d7a70297a523e7853f6a072ffb81d4f9af89a5
Javoxir G'ulomjonov <javohirtech@gmail.com>
2025-12-11 00:08:37 +0500

    chore: remove Nix sandbox configuration from Hetzner setup

--- hosts/hetzner/configuration.nix
+++ hosts/hetzner/configuration.nix
@@ -118,9 +118,5 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
-
-  # Disable Nix sandbox
-  nix.settings.sandbox = false;
-
 }
 
