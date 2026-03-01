8fadfaf483c02445ec6bbdc2eedcdb5f44b784c9
3nln <javohirtech@gmail.com>
2026-03-01 13:34:51 +0500

    feat: add launchd user agents for JetBrains Toolbox, Flameshot, Mac Fan Control, and Raycast

--- modules/darwin/configuration.nix
+++ modules/darwin/configuration.nix
@@ -33,5 +33,39 @@
     home = "/Users/nnolan";
   };
 
+  launchd.user.agents = {
+    jetbrains-toolbox = {
+      serviceConfig = {
+        ProgramArguments = ["/usr/local/bin/jetbrains-toolbox" "--minimize"];
+        RunAtLoad = true;
+        KeepAlive = false;
+      };
+    };
+
+    flameshot = {
+      serviceConfig = {
+        ProgramArguments = ["${pkgs.flameshot}/bin/flameshot"];
+        RunAtLoad = true;
+        KeepAlive = false;
+      };
+    };
+
+    macfancontrol = {
+      serviceConfig = {
+        ProgramArguments = ["/Applications/Macs Fan Control.app/Contents/MacOS/Macs Fan Control"];
+        RunAtLoad = true;
+        KeepAlive = false;
+      };
+    };
+
+    raycast = {
+      serviceConfig = {
+        ProgramArguments = ["/Applications/Raycast.app/Contents/MacOS/Raycast"];
+        RunAtLoad = true;
+        KeepAlive = false;
+      };
+    };
+  };
+
   system.stateVersion = 6;
 }
